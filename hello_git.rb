2b5d5f2db45143f7bec25caf2fc5e9bb620d45a8
storycoder <joseph.margolis@gmail.com>
2015-05-06 13:16:21 +0000

    second commit

---
+++ hello_git.rb
@@ -0,0 +1,10 @@
+puts "What's your name"
+name = gets.chomp.capitalize
+puts "Hi #{name}! My name is Git."
+
+# echo "# hello_git" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin git@github.com:storycoder/hello_git.git
+# git push -u origin master
